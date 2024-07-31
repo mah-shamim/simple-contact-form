d36d5c33b84e750086c6ea4c418e0e6d6a78ff03
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-08-01 01:31:08 +0600

    add database.sql

---
+++ database.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE guestbook_db;
+
+USE guestbook_db;
+
+CREATE TABLE contacts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    message TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
