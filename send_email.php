b968a995c3ae8c38e814bac1518b108a512dbd0c
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-08-01 02:50:41 +0600

    add send_email.php

---
+++ send_email.php
@@ -0,0 +1,8 @@
+<?php
+// Update these settings with your email configuration
+$to = 'mah.shamim@gmail.com';
+$subject = 'New Contact Form Message';
+$headers = 'From: mah.shamim@gmail.com' . "\r\n" .
+    'Reply-To: mah.shamim@gmail.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+?>
