14a506e6e438814b51a769881d9305444932c55d
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-08-01 02:50:25 +0600

    add script.js

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $('#contactForm').on('submit', function(event) {
+        event.preventDefault();
+        alert($(this).serialize());
+
+        $.ajax({
+            url: 'process_form.php',
+            type: 'POST',
+            data: $(this).serialize(),
+            success: function(response) {
+                alert('Message sent successfully!');
+                //$('#contactForm')[0].reset();
+            },
+            error: function(xhr, status, error) {
+                alert('An error occurred: ' + error);
+            }
+        });
+    });
+});
