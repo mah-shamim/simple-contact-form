fc51cd515097b816da33c3d7686afd9f2f22a095
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-08-01 02:51:57 +0600

    update db.php

--- db.php
+++ db.php
@@ -1,14 +1,15 @@
 <?php
 include 'config.php';
 // Database configuration
-$dsn = 'mysql:host='.$host.';dbname='.$dbname;
+$dsn = 'mysql:host='.DB_HOST.';dbname='.DB_NAME;
 $options = [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 ];
 
 try {
-    $pdo = new PDO($dsn, $username, $password, $options);
+    $pdo = new PDO($dsn, DB_USER, DB_PASS, $options);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die('Database connection failed: ' . $e->getMessage());
 }
